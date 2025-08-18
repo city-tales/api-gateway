b69c5b6149882d17d8d0da2b79d57e9e44ca02c3
ankit-anand-shipsy <ankit.anand@shipsy.io>
2025-08-18 16:06:41 +0530

    add health check api

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import "./config/init_connections.js";
 import { port } from "./config/config.js";
 import { server } from "./config/server.js";
 import { authenticationRouter } from "./routes/authentication.js";
+import { healthCheckRouter } from "./routes/healthcheck.js";
 import { Constants } from './utils/constants.js';
 
 server.use(`${Constants.ROUTES.AUTHENTICATION}`, authenticationRouter);
+server.use(`${Constants.ROUTES.HEALTHCHECK}`, healthCheckRouter);
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        message: `API Gateway running on port ${port}`
+    })
+});
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
